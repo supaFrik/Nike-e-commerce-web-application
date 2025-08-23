295aec5f9b370d9eb5e66c57353407bc9c7a4c51
supaFrik <tqv2005business@gmail.com>
2025-08-23 23:27:43 +0700

    Getter & setter for ProductVariant

--- src/main/java/vn/devpro/javaweb32/entity/product/ProductVariant.java
+++ src/main/java/vn/devpro/javaweb32/entity/product/ProductVariant.java
@@ -22,4 +22,60 @@ public class ProductVariant {
     private String color;
     private Integer stock;
     private BigDecimal price;
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public List<ProductVariant> getVariants() {
+        return variants;
+    }
+
+    public void setVariants(List<ProductVariant> variants) {
+        this.variants = variants;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
