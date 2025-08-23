91ae04a334417b7f21894b95d4af2c309c6d3ed5
supaFrik <tqv2005business@gmail.com>
2025-08-23 21:57:00 +0700

    Created category.java

--- src/main/java/vn/devpro/javaweb32/entity/product/Category.java
+++ src/main/java/vn/devpro/javaweb32/entity/product/Category.java
@@ -2,6 +2,18 @@ package vn.devpro.javaweb32.entity.product;
 
 import vn.devpro.javaweb32.entity.base.BaseModel;
 
-public class Category extends BaseModel {
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+@Table(name = "categories")
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private  String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 }
