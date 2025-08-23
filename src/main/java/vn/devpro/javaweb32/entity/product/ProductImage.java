21d5461ec6fff18587b1811365931ce21eaf164a
supaFrik <tqv2005business@gmail.com>
2025-08-23 23:27:21 +0700

    Getter & setter for ProductImage

--- src/main/java/vn/devpro/javaweb32/entity/product/ProductImage.java
+++ src/main/java/vn/devpro/javaweb32/entity/product/ProductImage.java
@@ -18,4 +18,28 @@ public class ProductImage {
     @ManyToOne
     @JoinColumn(name = "product_id")
     private Product product;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
