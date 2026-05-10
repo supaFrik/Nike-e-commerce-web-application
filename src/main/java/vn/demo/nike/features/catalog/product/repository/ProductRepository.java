cba7034a5ce5376ef1289058972483a52fa5202e
supaFrik <tqv2005business@gmail.com>
2026-05-11 00:11:23 +0700

    feat: optimizing the process of fetching product from DB by bit

--- src/main/java/vn/demo/nike/features/catalog/product/repository/ProductRepository.java
+++ src/main/java/vn/demo/nike/features/catalog/product/repository/ProductRepository.java
@@ -1,8 +1,6 @@
 package vn.demo.nike.features.catalog.product.repository;
 
-import jakarta.persistence.Entity;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -16,40 +14,35 @@ import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @EntityGraph(attributePaths = {
-            "category",
-            "colors",
-            "colors.images",
-            "colors.variants"
-    })
+    @EntityGraph(attributePaths = {"category", "colors"})
     Optional<Product> findDetailById(Long id);
 
     @Query("""
-    SELECT new vn.demo.nike.features.catalog.product.dto.request.ProductListItemView(
-        p.id,
-        p.name,
-        p.price,
-        p.salePrice,
-        CASE 
-            WHEN p.salePrice IS NOT NULL AND p.salePrice > 0 
-            THEN true 
-            ELSE false 
-        END,
-        p.productStatus,
-        p.type,
-        COALESCE(c.name, 'Uncategorized'),
-        (
-            SELECT MIN(pi.url)
-            FROM ProductImage pi
-            WHERE pi.color.product.id = p.id
-            AND pi.isMainForColor = true
-        ),
-        SIZE(p.colors)
-    )
-    FROM Product p
-    LEFT JOIN p.category c
-    WHERE (:categoryId IS NULL OR c.id = :categoryId)
-""")
+                SELECT new vn.demo.nike.features.catalog.product.dto.request.ProductListItemView(
+                    p.id,
+                    p.name,
+                    p.price,
+                    p.salePrice,
+                    CASE 
+                        WHEN p.salePrice IS NOT NULL AND p.salePrice > 0 
+                        THEN true 
+                        ELSE false 
+                    END,
+                    p.productStatus,
+                    p.type,
+                    COALESCE(c.name, 'Uncategorized'),
+                    (
+                        SELECT MIN(pi.url)
+                        FROM ProductImage pi
+                        WHERE pi.color.product.id = p.id
+                        AND pi.isMainForColor = true
+                    ),
+                    SIZE(p.colors)
+                )
+                FROM Product p
+                LEFT JOIN p.category c
+                WHERE (:categoryId IS NULL OR c.id = :categoryId)
+            """)
     List<ProductListItemView> findProductList(@Param("categoryId") Long categoryId, Pageable pageable);
 
     @EntityGraph(attributePaths = {"colors", "colors.images"})
--- src/main/java/vn/demo/nike/features/catalog/product/service/ProductDetailService.java
+++ src/main/java/vn/demo/nike/features/catalog/product/service/ProductDetailService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import vn.demo.nike.features.catalog.product.dto.response.ProductDetailResponse;
 import vn.demo.nike.features.catalog.product.dto.response.ProductQueryResponseMapper;
+import vn.demo.nike.features.catalog.product.entity.Product;
 import vn.demo.nike.features.catalog.product.exception.ProductNotFoundException;
 import vn.demo.nike.features.catalog.product.repository.ProductRepository;
 
@@ -17,8 +18,12 @@ public class ProductDetailService {
 
     @Transactional(readOnly = true)
     public ProductDetailResponse getProductDetail(Long id) {
-        return productRepository.findDetailById(id)
-                .map(productQueryResponseMapper::toProductDetailResponse)
+        Product product = productRepository.findDetailById(id)
                 .orElseThrow(() -> new ProductNotFoundException(id));
+        product.getColors().forEach(color -> {
+            color.getImages().size();
+            color.getVariants().size();
+        });
+        return productQueryResponseMapper.toProductDetailResponse(product);
     }
 }
